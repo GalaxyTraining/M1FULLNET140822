39c35c8a1044983a1709440f6d5e95a28fcd8dcb
Anthony Jesus Portilla Cano <aportilla@externo.auna.pe>
2022-08-14 14:33:55 -0500

    first commit

---
+++ MinimalApi/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace MinimalApi
+{
+    public class Product
+    {
+        public string?  Name { get; set; }
+
+        public string? TypeProduct { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
